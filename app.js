8bb66825fcb29ffb556ecc10927a00e19a2e5f14
Victor <lucckyboy713@gmail.com>
2021-07-22 21:29:58 +0300

    Finish

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+console.log('hurma!!!!!!!!!!!!!!')
+
 const musicContainer = document.getElementById('music-container')
 
-const playBtn = document.getElementById('play')
-const prevBtn = document.getElementById('prev')
-const nextBtn = document.getElementById('next')
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-const audio = document.getElementById('audio')
-const progress = document.getElementById('progress')
-const progressContainer = document.getElementById('progress-container')
-const title = document.getElementById('title')
-const cover = document.getElementById('cover')
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
 
 //Song Titles
 const songs = ['gospel','search','nazare'];
 
 // Keep track of song
-let songIndex = 0;
+let songIndex = 1;
 
 //Load song details into DOM
 loadSong(songs[songIndex]);
@@ -24,5 +26,85 @@ function loadSong(song){
     title.innerText = song;
     audio.src = `music/${song}.mp3`;
     cover.src = `img/${song}.jpg`;
+ }
 
+//Play Song 
+function playSong(){
+    musicContainer.classList.add("play");
+    playBtn.querySelector("i.fas").classList.remove("fa-play");
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+    audio.play();
 }
+
+//Pause Song
+function pauseSong(){
+musicContainer.classList.remove("play");
+playBtn.querySelector("i.fas").classList.add("fa-play");
+playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+audio.pause();
+}
+
+//Previous Song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+           
+    playSong();
+}
+
+//Previous Song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length-1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+           
+    playSong();
+}
+
+//Update Progress 
+function updateProgress(e){
+    const{duration,currentTime} = e.srcElement;
+    const progressPercent = (currentTime/duration)*100;
+    progress.style.width = `${progressPercent}%`
+}
+
+//Set Progress Bar
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX/width) * duration;
+}
+
+//Event Listener
+playBtn.addEventListener('click',() => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying){
+        pauseSong();
+    }
+    else{
+        playSong();
+    }
+});
+
+// Change Song
+prevBtn.addEventListener("click",prevSong);
+nextBtn.addEventListener("click",nextSong);
+
+//Time&SOng Update
+audio.addEventListener("timeupdate",updateProgress)
+
+//Clicl on progress
+progressContainer.addEventListener('click', setProgress);
+
+//Song Ends
+audio.addEventListener("ended",nextSong)
